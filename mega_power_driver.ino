d56e0a12404857b883b2520e9e0041fa857f0b96
ShewaEger <shewaeger@gmail.com>
2019-10-13 14:27:06 +0300

    getPower

--- mega_power_driver.ino
+++ mega_power_driver.ino
@@ -1,63 +1,19 @@
 #include <Arduino.h>
 #include "User_Setup.h"
 
-#define TIME_TO_DOWN 0x4AE0
+int analogPin = 3;     // номер порта к которому подключен потенциометр
+int val = 0;           // переменная для хранения считываемого значения
 
-#define TIME_10_uS 0x0015
+void setup()
+{
 
-#define START_TIMER (TCCR1B = (1 << CS11))
-#define STOP_TIMER (TCCR1B = 0)
-
-#define POWER_PIN 36
-
-uint16_t dimming = 0;
-
-void null_up() {
-    OCR1A = TIME_TO_DOWN - dimming;
-    OCR1B = OCR1A + TIME_10_uS;
-    TCNT1 = 0;
-    START_TIMER;
-}
-
-ISR(TIMER1_COMPA_vect) {
-    digitalWrite(POWER_PIN, 1);
-}
-
-ISR(TIMER1_COMPB_vect) {
-    digitalWrite(POWER_PIN, 0);
-    STOP_TIMER;
-}
-
-void setup() {
-
-
-    cli(); // отключить глобальные прерывания
-
-    TCCR1A = 0;
-    TCCR1B = 0;
-
-    TIMSK1 = (1 << OCIE1A) | (1 << OCIE1B);
-
-    pinMode(POWER_PIN, OUTPUT);
-    attachInterrupt(INT2, null_up, RISING);
+    analogReference(INTERNAL1V1);
     Serial.begin(9600);
-
-    sei();
 }
 
-void loop() {
-    int available = Serial.available();
-    char input[80];
-    if (!available)
-        return;
-
-    size_t reading = Serial.readBytesUntil('\n', input, 79);
-    input[reading] = 0;
-
-    long value = strtol(input, NULL, 10);
-    if(value < 0)
-        value = 0;
-    if(value > 96)
-        value = 96;
-    dimming = (TIME_TO_DOWN / 100) * value;
+void loop()
+{
+    val = analogRead(analogPin);     // считываем значение
+    Serial.println(val);
+    Serial.println((1.1 / 1024.) * val);             // выводим полученное значение
 }
