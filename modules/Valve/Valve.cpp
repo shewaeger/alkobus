ea831e006869f9f20bf8e32fdf3af899079f8c0f
ShewaEger <shewaeger@gmail.com>
2020-02-02 12:26:29 +0300

    add Valve class

---
+++ modules/Valve/Valve.cpp
@@ -0,0 +1,88 @@
+//
+// Created by shewa on 31.12.19.
+//
+
+#include <Arduino.h>
+#include "Valve.h"
+
+
+
+void Valve::close() {
+    digitalWrite(pin, !mode);
+}
+
+void Valve::open() {
+    digitalWrite(pin, mode);
+}
+
+Valve::Valve(int pin, bool mode) {
+    this->pin = pin;
+    this->mode = mode;
+    this->close();
+    pinMode(this->pin, OUTPUT);
+
+}
+
+Valve::~Valve() {
+
+}
+
+void Valve::setPWM(uint64_t pwmCount, bool pwmScale) {
+    this->pwmCount = pwmCount;
+    this->pwmScale = pwmScale;
+    if(!pwmScale)
+        this->pwm = 60000 / pwmCount;
+    else
+        this->pwm = 3600000 / pwmCount;
+}
+
+void Valve::setOpeningDuration(uint64_t duration) {
+    if(pwm < duration)
+        duration = pwm;
+    this->openingDuration = duration;
+}
+
+void Valve::loop() {
+    if(!pwmStatus)
+        return;
+    if(this->pwm == 0 || this->openingDuration == 0) {
+        this->close();
+        return;
+    }
+    if(this->openingDuration == this->pwm){
+        this->open();
+        return;
+    }
+    uint64_t millis_t = millis();
+    if(millis_t - lastPWM  >= pwm){
+        this->open();
+        this->lastPWM = millis_t;
+    }
+    if(millis_t - lastPWM >= openingDuration)
+        this->close();
+
+
+}
+
+uint64_t Valve::getOpeningDuration() {
+    return this->openingDuration;
+}
+
+uint64_t Valve::getPWMScale() {
+    return this->pwmScale;
+}
+
+uint64_t Valve::getPWMCount() {
+    return this->pwmCount;
+}
+
+bool Valve::switchPWMStatus() {
+    this->pwmStatus = !this->pwmStatus;
+    return this->pwmStatus;
+}
+
+void Valve::setPWMStatus(bool status) {
+    this->pwmStatus = status;
+}
+
+
